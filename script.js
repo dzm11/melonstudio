03d02447911f4cea129e6510642bdd181252c80e
Piotr Brzezinski <piotrbrzezinski@CLM23-POZ-PB.local>
2020-09-07 15:09:51 +0200

    porpawiony js

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
+// gsap.registerPlugin(ScrollTrigger);
+
+// const container = document.querySelector('#scroll-container');
+// const height = container.clientHeight;
+// document.body.style.height = `${height}px`;
+
+// gsap.to(container, {
+// 	y: -(height - document.documentElement.clientHeight),
+// 	ease: 'none',
+// 	scrollTrigger: {
+// 		trigger: document.body,
+// 		start: 'top top',
+// 		end: 'bottom bottom',
+// 		scrub: 1
+// 	}
+// });
+
 gsap.registerPlugin(ScrollTrigger);
 
-const container = document.querySelector('#scroll-container');
-const height = container.clientHeight;
-document.body.style.height = `${height}px`;
+var container = document.querySelector('#scroll-container');
+
+var height;
+function setHeight() {
+	height = container.clientHeight;
+	document.body.style.height = height + 'px';
+}
+ScrollTrigger.addEventListener('refreshInit', setHeight);
 
 gsap.to(container, {
-	y: -(height - document.documentElement.clientHeight),
+	y: () => -(height - document.documentElement.clientHeight),
 	ease: 'none',
 	scrollTrigger: {
 		trigger: document.body,
 		start: 'top top',
 		end: 'bottom bottom',
-		scrub: 1
+		scrub: 1,
+		invalidateOnRefresh: true
 	}
 });
